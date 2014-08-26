891fc9d15c109939d01fe34132c49fa5a9ecdb6e
Nathan Cahill <nathan@nathancahill.com>
2014-08-25 19:21:53 -0600

    pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Flask-Edits',
-    version='0.3',
-    packages=['flask_edits',],
-    license='BSD',
-    url='https://pypi.python.org/pypi/Flask-Edits',
-    author='Nathan Cahill',
-    author_email='nathan@nathancahill.com',
-    description='Editable Content in Flask',
-    long_description=open('README').read(),
+    name = 'Flask-Edits',
+    version = '0.8',
+    packages = find_packages(),
+    install_requires = ['Flask>=0.7',],
+    license = 'BSD',
+    url = 'http://github.com/nathancahill/flask-edits',
+    author = 'Nathan Cahill',
+    author_email = 'nathan@nathancahill.com',
+    description = 'Editable Content in Flask',
+    long_description = open('README').read(),
+    include_package_data=True,
 )
